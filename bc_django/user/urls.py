175afe022d302d628d196b5147a9bf29b4a43246
hhwrxhh <ivan.bohdan88y@gmail.com>
2022-10-03 12:32:18 +0300

    user profile

--- bc_django/user/urls.py
+++ bc_django/user/urls.py
@@ -6,4 +6,9 @@ urlpatterns = (
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('change_password/', change_password,
+         name='change_password'),
+    path('change_name/', change_name,
+         name='change_name'),
+
 )
--- bc_django/user/views.py
+++ bc_django/user/views.py
@@ -5,10 +5,21 @@ from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.shortcuts import redirect
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import PasswordChangeForm
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
 # Create your views here.
 
 def userpage(request):
-    return render(request, 'user/userpage.html')
+    user = get_user_model()
+    users = request.user
+    return render(request, 'user/userpage.html', {'res': users})
+
 
 def register(request):
     if request.method == 'POST':
@@ -16,14 +27,15 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Register complete')
-            return redirect('https://youtu.be/dQw4w9WgXcQ')
+            return redirect('')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {"form": form})
 
+
 def user_login(request):
     if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
+        form = UserLoginForm(data=reques.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
@@ -32,6 +44,39 @@ def user_login(request):
         form = UserLoginForm()
     return render(request, 'user/login.html', {"form": form})
 
+
 def user_logout(request):
     logout(request)
     return redirect('')
+
+
+# user profile
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('userpage')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    args = {'form': form}
+    return render(request, 'user/change_password.html', args)
+
+
+@login_required
+def change_name(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your name was successfully updated!')
+            return redirect('userpage')
+    else:
+        form = UserChangeForm(instance=request.user)
+
+    args = {'form': form}
+    return render(request, 'user/change_name.html', args)
+
