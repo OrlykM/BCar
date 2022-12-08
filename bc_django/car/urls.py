9e25d89fd401bb85c5f89002a39b49aacd055408
hhwrxhh <113522372+hhwrxhh@users.noreply.github.com>
2022-12-08 22:41:41 +0200

    added new url/serializatirs/validators:

--- bc_django/car/urls.py
+++ bc_django/car/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from user.views import *
 from car.views import *
 
-urlpatterns = (
-    path('search', CarViewFilterSet.as_view(), name='car_search'),
-    #path('list/', ''),
-    #path('single/', ''),
-    #path('add/', ''),
-    #path('approve/', ''),
-    #path('order/', ''),
-    #path('review/', ''),
-)
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('auth/', CustomAuthToken.as_view()),
+    #path('', include('main.urls')),
+    path('user/', include('user.urls')),
+    path('car/', include('car.urls')),
+
+]
